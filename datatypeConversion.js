95f2c8b8f526ad5ecf998f80513796b00342bebf
ayushsangamm <ayushsangam4u@gmail.com>
2026-03-13 17:36:14 +0530

    ch

--- datatypeConversion.js
+++ datatypeConversion.js
@@ -1,4 +1,29 @@
-let score = 33
+let score = undefined
 
-console.log(typeof score);
-console.log(typeof(score))
+//console.log(typeof score);
+//console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+//"33" => 33
+//"33abc" => Nan
+// true =>1; false => 0;
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+//1 => true; 0=> false
+
+//"hitesh" => true
+
+let somenumber = 33
+
+let stringnumber = String(somenumber)
+console.log(stringnumber)
+console.log(typeof stringnumber)
