c8ab1c3a0136352a10f366288d3b51c59d7a40d5
ayush sangam <144928497+ayushsangamm@users.noreply.github.com>
2026-03-13 17:13:45 +0530

    Add files via upload

---
+++ datatypes_and_ECMA.js
@@ -0,0 +1,26 @@
+"use strict";// treat all js code as newer version
+
+//alert(3 + 3) ye conole me kaam nai krega inspect element se kaam krega
+
+console.log(3 + 3) //code readabilty must be high
+console.log("Ayush")
+
+
+let name = "Ayush "
+let age = 18
+let isLoggedIn = false
+let state = null
+
+//number => 2 to power 53
+//bigint
+//string =>""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => uniqueness
+
+
+//object 
+console.log(typeof undefined)//undefined
+console.log(typeof null) //object
+ 
